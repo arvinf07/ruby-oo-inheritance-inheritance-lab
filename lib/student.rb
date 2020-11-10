914d93dedabff028e830ca65adfce7eec448640f
Arvin Fernandez <arvinf07@gmail.com>
2020-11-09 19:52:32 -0500

    commit tree 27cb673cdf3f8958312c5ebfe6fc0b5923701b33

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative 'user.rb'
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+    @@all << self
+  end  
+
+  def learn(topic)
+    @knowledge << topic
+  end  
+
 
 end
